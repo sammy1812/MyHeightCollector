33075cb7d13088073c0deeebb13b2615fab68508
whateverusername <samikrajaram@yahoo.com>
2021-10-04 07:54:17 +0530

    changed_url

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from send_email import send_email
 from sqlalchemy.sql import func
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:postgres123@localhost/height_calculator'
+app.config['SQLALCHEMY_DATABASE_URI']='postgres://xbbjsspscgmysg:7f1a9f17d64c014ec4769316fe81db7cb2aa08131a84414159c3959fed22efcb@ec2-34-202-7-83.compute-1.amazonaws.com:5432/da6g60jf9f7an6?sslmode=require'
 db = SQLAlchemy(app)
 class Data(db.Model):
     __tablename__ = "data"
